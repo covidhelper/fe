3187af7d15235ad20f28bfa8ecb504e171d1aab2
Atharva J <caplock221b@gmail.com>
2021-04-20 01:18:08 +0530

    changed base url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/"
+export const BASE_URL = "https://covid19helpers.in/"
 
 // Data Upload
 export const FORM_FILL_UNSTRUCTURED = BASE_URL + "v1/data/unstructured" // POST
