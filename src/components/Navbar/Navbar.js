bc3c27708f66128defa70f2e156891405e370b68
Rachit Puri <rachitpuri@Rachits-MacBook-Air.local>
2021-04-20 22:13:05 +0530

    Navbar changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -24,10 +24,10 @@ const Navbar = () => {
                     <Link to="/">Home</Link>
                 </li>
                 <li className={`nav-li ${location.pathname === "/get-help" ? ' active' : null}`}>
-                    <Link to="/get-help">Add Info</Link>
+                    <Link to="/get-help">Get Info</Link>
                 </li>
                 <li className={`nav-li ${location.pathname === "/contribute" ? ' active' : null}`}>
-                    <Link to="/contribute">Get Info</Link>
+                    <Link to="/contribute">Add Info</Link>
                 </li>
             </ul>
         </nav>
